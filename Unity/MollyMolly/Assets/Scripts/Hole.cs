ddf8297a26f9dc9f43f63d97e2dd3bdc0312bb94
KMW <dndaks1304@gmail.com>
2020-07-18 17:10:21 +0900

    Unity_MollyMolly : 프로젝트 생성

---
+++ Unity/MollyMolly/Assets/Scripts/Hole.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum MoleState
+{
+    NONE,
+    OPEN,
+    IDLE,
+    CLOSE,
+    CATCH,
+    END
+}
+
+public class Hole : MonoBehaviour
+{
+    public MoleState state;
+}
